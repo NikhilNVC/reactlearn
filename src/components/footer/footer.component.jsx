3b844528a6845e148516b0c52fb5aeabde634965
NikhilNVC <nikhil.chinnala@gmail.com>
2023-02-08 08:38:44 -0500

    Added routes

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,14 @@
+import './footer.styles.scss';
+
+const Footer = () =>{
+
+    return(
+        <div className="footer">
+                <div>Property of RR Clothing</div>
+                <div>©RR Clothing Enterprises</div>
+                <div>Contact Us</div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import Footer from "../../components/footer/footer.component";
+import { ReactComponent as Logo} from "./../../assets/crown.svg";
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return(
+      <>
+        <div>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    <Logo className="logo"/>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>SHOP</Link>
+                </div>           
+            
+            </div>
+
+            <Outlet/>
+            <Footer/>
+        </div>
+      </>
+    );
+  }
+
+  export default Navigation;
